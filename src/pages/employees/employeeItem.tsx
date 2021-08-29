24f7bd7f5b1e9d1ab5a5bd348361f09e03ce9414
asma mir <asmamir297@gmail.com>
2021-08-29 09:54:18 +0430

    Adds the employees list page

---
+++ src/pages/employees/employeeItem.tsx
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Grid from 'components/Grid';
+import Button from 'components/Button';
+import Typography from 'components/Typography';
+import TrashIcon from 'components/Icons/Trash';
+import EyeIcon from 'components/Icons/Eye';
+
+type EmployeeType = {
+    first_name: string
+    last_name: string
+    _id: string
+    role: string
+}
+
+function employeeToString({ first_name, last_name }: EmployeeType) {
+    return `${first_name} ${last_name}`;
+}
+
+const EmployeeItemWrapper = styled.li`
+    padding: ${({theme}) => theme.sizes.toRem(10)} 0;
+
+    &:not(:first-child) {
+        border-top: 1px solid ${({theme}) => theme.colors.primary[700]};
+    }
+
+    p {
+        font-size: ${({theme}) => theme.sizes.toRem(20)};
+        font-weight: 600;
+    }
+
+    span {
+        font-weight: 500;
+    }
+`;
+
+function EmployeeItem({ employee }: { employee: EmployeeType }) {
+    return (
+        <EmployeeItemWrapper>
+            <Grid container justify="space-between" align="center">
+                <div>
+                    <Typography>{employeeToString(employee)}</Typography>
+                    <Typography component="span">{employee.role}</Typography>
+                </div>
+                <Grid container>
+                    <Button variant="outlined" color="primary"> <TrashIcon /> </Button>
+                    <Link to={`/${employee._id}/edit`}>
+                        <Button variant="outlined" color="primary"> <EyeIcon /> </Button>
+                    </Link>
+                </Grid>
+            </Grid>
+        </EmployeeItemWrapper>
+    )
+}
+
+export default EmployeeItem;
--- src/pages/employees/employees.tsx
+++ src/pages/employees/employees.tsx
@@ -1,10 +1,31 @@
+import EmployeeItem from './employeeItem';
+
 function HomePage() {
 
     // TODO -> Requests to get a list of employees to show
 
     return (
-        <div> List of Employees </div>
+        <div>
+            <ul>
+                {FAKE_DATA.map(employee => <EmployeeItem key={employee._id} employee={employee} />)}
+            </ul>
+        </div>
     )
 }
 
 export default HomePage;
+
+const FAKE_DATA = [
+    {
+        _id: "611a20efabc1136a29eec153",
+        first_name: "Maloooooooooooooch",
+        last_name: "Mir",
+        role: "Backend Engineer"
+    },
+    {
+        _id: "611a30f8f078501e1ced149a",
+        first_name: "Solmaz",
+        last_name: "Khosravi",
+        role: "Fullstack Engineer"
+    }
+]
