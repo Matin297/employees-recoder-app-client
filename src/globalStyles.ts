c1cfd05201abc6b8a621c8644b6b39b3dbb514d1
asma mir <asmamir297@gmail.com>
2021-08-29 10:13:50 +0430

    Adds reset global styles

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -19,7 +19,34 @@ const GlobalStyles = createGlobalStyle`
         font-family: 'Montserrat', sans-serif;
     }
 
-    h1 {
+    button {
+        font-size: 16px;
+        border: none;
+        background-color: transparent;
+        border-radius: 5px;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: filter 200ms ease-in-out;
+        padding: 10px;
+    }
+
+    button:hover {
+        filter: brightness(130%);
+    }
+
+    span {
+        display: inline-block;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    h1, h2, h3, h4, h5, p {
         margin: 0;
         padding: 0;
     }
